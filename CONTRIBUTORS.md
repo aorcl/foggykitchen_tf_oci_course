c0dadcb99f021f621e1fbdbd8c1ce51d31682ba7
Badscrew <badscrew@dolganoff.com>
2020-04-30 10:59:00 +0200

    cleanup unused lessons, add CONTRIBUTORS.md

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,2 @@
+original author: Luke Martin Feldman (aka Martin Linxfeld) https://github.com/mlinxfeld 
+modifications for EMEA CCOE: Alexey Dolganov https://github.com/aorcl
--- LESSON7a_dbsystem_with_dataguard/compartment.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_identity_compartment" "FoggyKitchenCompartment" {
-  name = "FoggyKitchenCompartment"
-  description = "FoggyKitchen Compartment"
-  compartment_id = var.compartment_ocid
-}
-
